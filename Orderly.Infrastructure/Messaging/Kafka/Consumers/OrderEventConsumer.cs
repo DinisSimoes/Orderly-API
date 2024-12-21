b0ab869dba430737b6929b28b89bbbd44a558f0d
DinisSimoes <di.f.simoes@gmail.com>
2024-12-20 21:27:19 -0300

    consumer and producer kafka

---
+++ Orderly.Infrastructure/Messaging/Kafka/Consumers/OrderEventConsumer.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Orderly.Domain.Entities;
+
+namespace Orderly.Infrastructure.Messaging.Kafka.Consumers
+{
+    public class OrderEventConsumer
+    {
+        private readonly IMongoCollection<Order> _orderCollection;
+        private readonly IConsumer<string, string> _consumer;
+
+        public OrderEventConsumer(IMongoCollection<Order> orderCollection, IConsumer<string, string> consumer)
+        {
+            _orderCollection = orderCollection;
+            _consumer = consumer;
+        }
+
+        public void StartConsuming()
+        {
+            _consumer.Subscribe("orders");
+
+            while (true)
+            {
+                var consumeResult = _consumer.Consume(CancellationToken.None);
+                var orderEvent = JsonConvert.DeserializeObject<Order>(consumeResult.Message.Value);
+
+                // Verifica se o pedido já existe no MongoDB
+                var existingOrder = _orderCollection.Find(o => o.Id == orderEvent.Id).FirstOrDefault();
+
+                if (existingOrder != null)
+                {
+                    // Atualiza o pedido existente
+                    _orderCollection.ReplaceOne(o => o.Id == orderEvent.Id, orderEvent);
+                }
+                else
+                {
+                    // Adiciona o novo pedido
+                    _orderCollection.InsertOne(orderEvent);
+                }
+            }
+        }
+    }
+}
---
+++ Orderly.Infrastructure/Messaging/Kafka/Producers/OrderEventProducer.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using Orderly.Domain.Entities;
+using Orderly.Domain.Interfaces.Messaging.Kafka;
+
+namespace Orderly.Infrastructure.Messaging.Kafka.Producers
+{
+    public class OrderEventProducer : IOrderEventProducer
+    {
+        private readonly IProducer<string, string> _producer;
+
+        public OrderEventProducer(IProducer<string, string> producer)
+        {
+            _producer = producer;
+        }
+
+        public async Task PublishOrderCreatedEvent(Order order)
+        {
+            var orderCreatedEvent = new
+            {
+                Id = order.Id,
+                CustomerId = order.CustomerId,
+                OrderDate = order.OrderDate,
+                TotalAmount = order.TotalAmount,
+                Status = order.Status
+            };
+
+            var message = new Message<string, string>
+            {
+                Key = order.Id.ToString(),
+                Value = JsonConvert.SerializeObject(orderCreatedEvent)
+            };
+
+            await _producer.ProduceAsync("orders", message);
+        }
+    }
+}
