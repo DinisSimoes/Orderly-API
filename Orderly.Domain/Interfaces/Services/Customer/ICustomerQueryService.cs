1ae33fd9f333b01f921ab9bc049f562b6c47b108
DinisSimoes <di.f.simoes@gmail.com>
2024-12-20 21:25:11 -0300

    Add customer Services

---
+++ Orderly.Domain/Interfaces/Services/Customer/ICustomerQueryService.cs
@@ -0,0 +1,9 @@
+﻿namespace Orderly.Domain.Interfaces.Services.Customer
+{
+    public interface ICustomerQueryService
+    {
+        Task<Entities.Customer> GetByIdAsync(Guid id);
+        Task<IEnumerable<Entities.Customer>> GetAllAsync();
+        Task<Entities.Customer> GetByEmailAsync(string email);
+    }
+}
