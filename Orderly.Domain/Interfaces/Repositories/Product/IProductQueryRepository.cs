5ba362b08e37bdb9e77f2144e461850449f48b22
DinisSimoes <di.f.simoes@gmail.com>
2024-12-20 21:24:56 -0300

    Add product repositories

---
+++ Orderly.Domain/Interfaces/Repositories/Product/IProductQueryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Orderly.Domain.Interfaces.Repositories.Product
+{
+    public interface IProductQueryRepository
+    {
+        Task<Entities.Product> GetByIdAsync(Guid id);
+        Task<IEnumerable<Entities.Product>> GetAllAsync();
+    }
+}
