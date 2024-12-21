3f40901636b40b1878f34dd32264a08f07c220e6
DinisSimoes <di.f.simoes@gmail.com>
2024-12-20 21:24:30 -0300

    Add order repositories

---
+++ Orderly.Domain/Interfaces/Repositories/Order/IOrderQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Orderly.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orderly.Domain.Interfaces.Repositories.Order
+{
+    public interface IOrderQueryRepository
+    {
+        Task<Entities.Order> GetByIdAsync(Guid id);
+        Task<IEnumerable<Entities.Order>> GetAllAsync();
+        Task<IEnumerable<Entities.Order>> GetByCustomerIdAsync(Guid customerId);
+    }
+}
