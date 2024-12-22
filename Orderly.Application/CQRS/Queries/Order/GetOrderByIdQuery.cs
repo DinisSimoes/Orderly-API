5f614d5ad40209321946280bdfdb2e8ec291c50d
DinisSimoes <di.f.simoes@gmail.com>
2024-12-21 23:56:44 -0300

    queries for CQRS

---
+++ Orderly.Application/CQRS/Queries/Order/GetOrderByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Orderly.Application.CQRS.Responses.Order;
+
+namespace Orderly.Application.CQRS.Queries.Order
+{
+    public class GetOrderByIdQuery : IRequest<OrderResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
