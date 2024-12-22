96a4162a574091cabd21ca8f18e1bc125c2e9244
DinisSimoes <di.f.simoes@gmail.com>
2024-12-21 23:56:33 -0300

    commands for CQRS

---
+++ Orderly.Application/CQRS/Commands/Order/CreateOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Orderly.Application.CQRS.Responses.Order;
+using Orderly.Application.DTOs;
+using Orderly.Domain.Entities;
+
+namespace Orderly.Application.CQRS.Commands.Order
+{
+    public class CreateOrderCommand : IRequest<Guid>
+    {
+        public List<OrderItemDto> OrderItems { get; set; } = new();
+    }
+}
---
+++ Orderly.Application/CQRS/Commands/Order/UpdateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Orderly.Application.DTOs;
+using Orderly.Domain.Entities;
+using System.Text.Json.Serialization;
+
+namespace Orderly.Application.CQRS.Commands.Order
+{
+    public class UpdateOrderCommand : IRequest<Unit>
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
