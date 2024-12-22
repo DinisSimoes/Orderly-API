87e77b23981cb3f97822debd8125c0d461ea0af4
DinisSimoes <di.f.simoes@gmail.com>
2024-12-21 23:58:35 -0300

    change to public interface

--- Orderly.Domain/Interfaces/Services/Order/IOrderQueryService.cs
+++ Orderly.Domain/Interfaces/Services/Order/IOrderQueryService.cs
@@ -1,6 +1,6 @@
 ﻿namespace Orderly.Domain.Interfaces.Services.Order
 {
-    internal interface IOrderQueryService
+    public interface IOrderQueryService
     {
         Task<Domain.Entities.Order> GetByIdAsync(Guid id);
         Task<IEnumerable<Domain.Entities.Order>> GetAllAsync();
