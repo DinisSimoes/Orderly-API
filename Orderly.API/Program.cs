c9f5fa2749e306b2603e91da318e5619a2b32289
DinisSimoes <di.f.simoes@gmail.com>
2024-12-22 00:03:46 -0300

    update file program

--- Orderly.API/Program.cs
+++ Orderly.API/Program.cs
@@ -1,9 +1,17 @@
+using Confluent.Kafka;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using MongoDB.Driver;
+using Orderly.Application.CQRS.Commands.Order;
+using Orderly.Application.CQRS.Handlers.Order;
+using Orderly.Application.Services.Order;
+using Orderly.Domain.Interfaces.Messaging.Kafka;
 using Orderly.Domain.Interfaces.Repositories.Customer;
 using Orderly.Domain.Interfaces.Repositories.Order;
 using Orderly.Domain.Interfaces.Repositories.OrderItem;
 using Orderly.Domain.Interfaces.Repositories.Product;
+using Orderly.Domain.Interfaces.Services.Order;
+using Orderly.Infrastructure.Messaging.Kafka.Producers;
 using Orderly.Infrastructure.Persistence;
 using Orderly.Infrastructure.Repositories.Customer;
 using Orderly.Infrastructure.Repositories.Order;
@@ -13,23 +21,55 @@ using Orderly.Infrastructure.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Swagger Configuration
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Orderly API", Version = "v1" });
+    c.EnableAnnotations();
+});
+
 // Add services to the container.
 //SQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
+//MongoDB
+builder.Services.AddScoped<MongoDbContext>(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("MongoDbSettings:ConnectionString");
+    var databaseName = configuration["MongoDbSettings:DatabaseName"];
+
+    return new MongoDbContext(connectionString, databaseName);
+});
+
+
+
 builder.Services.AddScoped<IProductCommandRepository, ProductCommandRepository>();
 builder.Services.AddScoped<IOrderItemCommandRepository, OrderItemCommandRepository>();
 builder.Services.AddScoped<ICustomerCommandRepository, CustomerCommandRepository>();
 builder.Services.AddScoped<IOrderCommandRepository, OrderCommandRepository>();
+builder.Services.AddScoped<IOrderQueryRepository, OrderQueryRepository>();
+builder.Services.AddScoped<IProductQueryRepository, ProductQueryRepository>();
 
-//MongoDB
-var mongoDbSettings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>();
-builder.Services.AddSingleton<IMongoClient, MongoClient>(sp => new MongoClient(mongoDbSettings.ConnectionString));
-builder.Services.AddSingleton(sp =>
-    sp.GetRequiredService<IMongoClient>().GetDatabase(mongoDbSettings.DatabaseName));
+
+builder.Services.AddScoped<IOrderCommandService, OrderCommandService>();
+builder.Services.AddScoped<IOrderQueryService, OrderQueryService>();
 
 //builder.Services.AddMediatR(typeof(Program).Assembly);
+
+
+// Configuração do Kafka
+builder.Services.AddSingleton(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    return new ProducerConfig
+    {
+        BootstrapServers = configuration["Kafka:BootstrapServers"] // Certifique-se de que este valor está definido no appsettings.json
+    };
+});
+
+builder.Services.AddSingleton<IOrderEventProducer, OrderEventProducer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -37,6 +77,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateOrderHandler).Assembly));
 
 var app = builder.Build();
 
